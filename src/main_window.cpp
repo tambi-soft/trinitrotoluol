9cc151734ad543c3496fa991fd3a961c7223e106
nano13 <nano13@gmx.net>
2019-08-21 15:58:28 +0200

    initialized qt project

---
+++ src/main_window.cpp
@@ -0,0 +1,9 @@
+
+#include "main_window.h"
+
+QTNTMainWindow::QTNTMainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    resize(1080, 1080);
+    
+}
---
+++ src/main_window.h
@@ -0,0 +1,21 @@
+
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QDir>
+#include <QIcon>
+#include <QMouseEvent>
+#include <QToolTip>
+
+class QTNTMainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    QTNTMainWindow(QWidget *parent = nullptr);
+    
+private:
+    
+    
+private slots:
+    
+};
