3c54e55a3dafc17f377d0d9865ee52709bfc9a47
nano13 <nano13@gmx.net>
2019-09-02 01:58:20 +0200

    sqlite sync: preparing db openings

--- src/sqlite_sync.cpp
+++ src/sqlite_sync.cpp
@@ -1,6 +1,35 @@
 #include "sqlite_sync.h"
 
-SQLiteSync::SQLiteSync()
+SQLiteSync::SQLiteSync(QString path_current_db, QString path_conflicting_db)
 {
+    // this has to be the same as "db_name" in class DbAdapter
+    QString name_db_current = "trinitro";
     
+    QString name_db_conflicting = "trinitro_conflicting";
+    
+    if (! QSqlDatabase::contains(name_db_current))
+    {
+        QSqlDatabase::addDatabase("QSQLITE", name_db_current);
+    }
+    
+    if (! QSqlDatabase::contains(name_db_conflicting))
+    {
+        QSqlDatabase::addDatabase("QSQLITE", name_db_conflicting);
+    }
+    
+    this->db_current = QSqlDatabase::database(name_db_current);
+    this->db_conflicting = QSqlDatabase::database(name_db_conflicting);
+    
+    this->db_current.setDatabaseName(path_current_db);
+    this->db_conflicting.setDatabaseName(path_conflicting_db);
+    
+    if (! this->db_current.open())
+    {
+        qDebug() << "Error: db current connection fail";
+    }
+    
+    if (! this->db_conflicting.open())
+    {
+        qDebug() << "Error: db conflicting connection fail";
+    }
 }
--- src/sqlite_sync.h
+++ src/sqlite_sync.h
@@ -2,11 +2,21 @@
 #define SQLITE_SYNC_H
 
 #include <QObject>
+#include <QDebug>
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
 
 class SQLiteSync
 {
 public:
-    SQLiteSync();
+    SQLiteSync(QString path_current_db, QString path_conflicting_db);
+    
+private:
+    QSqlDatabase db_current;
+    QSqlDatabase db_conflicting;
 };
 
 #endif // SQLITE_SYNC_H
