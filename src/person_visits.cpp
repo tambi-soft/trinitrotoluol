ad43e270f306c143c66a304efcd3be28dd264e2e
nano13 <nano13@gmx.net>
2020-02-09 21:19:31 +0100

    person_visits: implemented deleting entries

--- src/person_visits.cpp
+++ src/person_visits.cpp
@@ -22,17 +22,38 @@ void PersonVisits::showData()
     for (int i=0; i < data.length(); ++i)
     {
         QMap<QString,QVariant> visit = data.at(i);
+        qlonglong rowid = visit["rowid"].toLongLong();
+        QString name = visit["journey_name"].toString();
         
         QPushButton *button_delete = new QPushButton();
         button_delete->setIcon(QIcon::fromTheme("edit-delete"));
+        button_delete->setToolTip("delete this entry");
+        connect(button_delete, &QPushButton::clicked, this, [this, rowid, name]{ PersonVisits::onDeleteButtonClicked(rowid, name); });
         
         QPushButton *button_edit = new QPushButton();
         button_edit->setIcon(QIcon::fromTheme("document-properties"));
+        button_edit->setToolTip("edit this entry");
+        connect(button_edit, &QPushButton::clicked, this, [this, rowid]{ PersonVisits::onEditButtonClicked(rowid); });
         
         this->grid->addWidget(button_delete, i+1, 0);
         this->grid->addWidget(button_edit, i+1, 1);
-        this->grid->addWidget(new QLabel(visit["journey_name"].toString()), i+1, 2);
+        this->grid->addWidget(new QLabel(name), i+1, 2);
         this->grid->addWidget(new QLabel(visit["date"].toString()), i+1, 3);
         this->grid->addWidget(new QLabel(visit["notes"].toString()), i+1, 4);
     }
+}
+
+void PersonVisits::onDeleteButtonClicked(qlonglong rowid, QString name)
+{
+    int reply = QMessageBox::question(this, "Delete "+name, "Really delete \""+name+"\"?", QMessageBox::Yes, QMessageBox::No);
+    if (reply == QMessageBox::Yes)
+    {
+        this->db->deleteVisit(rowid);
+        showData();
+    }
+}
+
+void PersonVisits::onEditButtonClicked(qlonglong rowid)
+{
+    
 }
--- src/person_visits.h
+++ src/person_visits.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QLabel>
 #include <QPushButton>
+#include <QMessageBox>
 
 #include "lib/grid_widget.h"
 #include "db_adapter.h"
@@ -19,6 +20,11 @@ private:
     qlonglong rowid_person;
     
     void showData();
+    
+private slots:
+    void onDeleteButtonClicked(qlonglong rowid, QString name);
+    void onEditButtonClicked(qlonglong rowid);
+    
 };
 
 #endif // PERSONVISITS_H
