c3fa878f0270119be5f98e03f0e3ac9c50683c2e
tambi-soft <nano13@gmx.net>
2020-07-07 23:53:09 +0200

    journey_list: show years and current date

--- src/journey_list.cpp
+++ src/journey_list.cpp
@@ -29,17 +29,22 @@ void JourneyList::showJourneys()
     
     // add headers
     this->grid->addWidget(new  QLabel("<b>journey name</b>"), 0, 2);
-    this->grid->addWidget(new  QLabel("<b>date from</b>"), 0, 3);
-    this->grid->addWidget(new  QLabel("<b>date to</b>"), 0, 4);
+    this->grid->addWidget(new  QLabel("<b>date from</b>"), 0, 3, Qt::AlignCenter);
+    this->grid->addWidget(new  QLabel("<b>date to</b>"), 0, 4, Qt::AlignCenter);
     this->grid->addWidget(new  QLabel("<b>total costs</b>"), 0, 5, Qt::AlignRight);
     
     QIcon icon_edit = QIcon::fromTheme("document-properties");
     QIcon icon_delete = QIcon::fromTheme("edit-delete");
+    int current_year = 0;
+    bool date_already_inserted = false;
+    int row = 1;
     for (int i=1; i < data.length()+1; ++i)
     {
         // for running from 1 -> ...+1 because of having the headers at line 0 in the grid
         // because of this, we need to -1 here for getting the iterating and the further positions in the grid right
         QMap<QString,QVariant> journey = data.at(i-1);
+        ++row;
+        //row = i;
         
         qlonglong rowid = journey["rowid"].toLongLong();
         QString name = journey["name"].toString();
@@ -54,21 +59,40 @@ void JourneyList::showJourneys()
         button_delete->setMaximumWidth(40);
         connect(button_delete, &QPushButton::clicked, this, [this, rowid, name]{ JourneyList::journeyDelete(rowid, name); });
         
-        QString date_from = QDate::fromString(journey["date_from"].toString(), "yyyy-MM-dd").toString();
+        QDate date_now = QDate::currentDate();
+        QDate date_from_ = QDate::fromString(journey["date_from"].toString(), "yyyy-MM-dd");
+        QString date_from = date_from_.toString();
         QString date_to = QDate::fromString(journey["date_to"].toString(), "yyyy-MM-dd").toString();
         
-        this->grid->addWidget(button_delete, i, 0);
-        this->grid->addWidget(button_edit, i, 1);
-        this->grid->addWidget(new QLabel(name), i, 2);
-        this->grid->addWidget(new QLabel(date_from), i, 3, Qt::AlignRight);
-        this->grid->addWidget(new QLabel(date_to), i, 4, Qt::AlignRight);
-        this->grid->addWidget(new QLabel(journey["costs"].toString()), i, 5, Qt::AlignRight);
-        this->grid->addWidget(new QLabel(currency_code_default), i, 6);
+        int year = QDate::fromString(journey["date_from"].toString(), "yyyy-MM-dd").year();
+        if (current_year != year)
+        {
+            current_year = year;
+            this->grid->addWidget(new QLabel("<i><b>"+QString::number(year)+"</i></b>"), row, 2);
+            ++row;
+        }
+        
+        if (!date_already_inserted && (date_from_ <= date_now))
+        {
+            this->grid->addWidget(new QLabel("<b><u>today</u></b>"), row, 2);
+            this->grid->addWidget(new QLabel("<b><u>"+date_now.toString()+"</b></u>"), row, 3, Qt::AlignRight);
+            date_already_inserted = true;
+            ++row;
+        }
+        
+        
+        this->grid->addWidget(button_delete, row, 0);
+        this->grid->addWidget(button_edit, row, 1);
+        this->grid->addWidget(new QLabel(name), row, 2);
+        this->grid->addWidget(new QLabel(date_from), row, 3, Qt::AlignRight);
+        this->grid->addWidget(new QLabel(date_to), row, 4, Qt::AlignRight);
+        this->grid->addWidget(new QLabel(journey["costs"].toString()), row, 5, Qt::AlignRight);
+        this->grid->addWidget(new QLabel(currency_code_default), row, 6);
     }
     // push all columns to the left for getting the table a bit more compact
     this->grid->setColumnStretch(100, 100);
     // push everything up
-    this->grid->setRowStretch(data.length()+1, 100);
+    this->grid->setRowStretch(data.length()+100, 100);
 }
 
 void JourneyList::journeyNew()
